8782424fe726ec506074075f60a61442af5bb450
yin360 <wimala.saet@bumail.net>
2024-04-28 18:03:40 +0700

    updatemap

--- Assets/Scrip/Bullet.cs
+++ Assets/Scrip/Bullet.cs
@@ -6,4 +6,22 @@ using UnityEngine.SceneManagement;
 public class Bullet : MonoBehaviour
 {
     
+    
+    public int damageAmount = 3;
+    public float destroyDelay = 0.05f;
+    
+
+    void TakeDamage(int damage)
+    {
+        Debug.Log("player tok" + damage + "damage!");
+    }
+    private void OnCollisionEnter2D(Collision2D collision2D)
+    {
+        if (collision2D.gameObject.CompareTag("Player") || collision2D.gameObject.CompareTag("Enemy"))
+        {
+            TakeDamage(damageAmount);
+            Destroy(gameObject , destroyDelay);
+            SceneManager.LoadScene("Lose");
+        }
+    }
 }
