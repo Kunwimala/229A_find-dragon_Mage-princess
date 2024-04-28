40fc3445a230028e607ea8357f4f2cf32b76d531
yin360 <wimala.saet@bumail.net>
2024-04-28 18:37:13 +0700

    addprojecttile

--- Assets/Scrip/Bullet.cs
+++ Assets/Scrip/Bullet.cs
@@ -10,7 +10,7 @@ public class Bullet : MonoBehaviour
     public int damageAmount = 3;
     public float destroyDelay = 0.05f;
     
-
+    
     void TakeDamage(int damage)
     {
         Debug.Log("player tok" + damage + "damage!");
--- Assets/Scrip/EnemyShoot.cs
+++ Assets/Scrip/EnemyShoot.cs
@@ -5,6 +5,7 @@ public class EnemyShoot : MonoBehaviour
     public GameObject projectilePrefab; // ประกาศตัวแปรเก็บ prefab ของ projectile
     public float shootForce = 10f; // ความเร็วในการยิง projectile
     public float shootingRange = 10f; // ระยะห่างสูงสุดที่จะยิง
+    [SerializeField] private Transform shootPoint;
 
     public Transform player; // เก็บ reference ของ player
 
@@ -31,7 +32,7 @@ public class EnemyShoot : MonoBehaviour
     {
         // สร้าง projectile จาก prefab ที่กำหนด
         GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
-        
+
         // ทำการยิง projectile ออกไป (ตัวอย่างเพิ่มเติม: ตั้งค่าความเร็วหรือทิศทางของ projectile ตามต้องการ)
         Rigidbody projectileRb = projectile.GetComponent<Rigidbody>();
         if (projectileRb != null)
@@ -42,3 +43,23 @@ public class EnemyShoot : MonoBehaviour
         }
     }
 }
+
+
+/* Vector2 projectile = CalculateprojectileVelocity(shootPoint.transform.position, hit.point, 1f);
+ Rigidbody2D fireBullet = Instantiate(bulletprefab, shootPoint.position, Quaternion.identity);
+ fireBullet.velocity = projectile;
+}
+}
+Vector2 CalculateprojectileVelocity(Vector2 origin, Vector2 target, float t)
+{
+Vector2 distance = target - origin;
+
+float disX = distance.x;
+float disY = distance.y;
+float velocityX = disX / t;
+float velocityY = disY / t + 0.5f * Mathf.Abs(Physics2D.gravity.y * t);
+
+Vector2 result = new Vector2(velocityX, velocityY);
+return result;
+}*/
+        
