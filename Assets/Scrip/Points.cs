c0753045a6d59b52fe13989c95bd6951445cbd02
Ekkarat02-lab <ekkarat.thai@bumail.net>
2024-04-28 17:02:10 +0700

    point+1

--- Assets/Scrip/Points.cs
+++ Assets/Scrip/Points.cs
@@ -1,27 +1,23 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class Points : MonoBehaviour
 {
-   private int score = 0;
-   [SerializeField] private TextMeshProUGUI points;
+    public TextMeshProUGUI pointsText;
+    private int points = 0;
 
-    void OnTriggerEnter2D(Collider2D other)
-   {
-      if ( other.CompareTag("Player"))
-      {
-         if (gameObject.CompareTag("Points"))
-         {
-            score += 1;
-            points.text = "Points:" + score;
-            
-            Debug.Log("player:" + score);
-            
-            Destroy(gameObject);
-         }
-      }
-   }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Points"))
+        {
+            points++;
+            UpdatePointsText();
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void UpdatePointsText()
+    {
+        pointsText.text = "Points: " + points.ToString();
+    }
 }
